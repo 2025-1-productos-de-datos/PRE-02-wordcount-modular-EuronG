6a86465f8c5978d1308b7c0b0f623bb765b4ba5e
EuronG <juandigiraldoj@gmail.com>
2025-05-05 00:15:44 -0500

    wordcount progress

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -5,20 +5,40 @@ from ._internals.write_count_words import write_count_words
 
 
 def main():
+
+    # read all lines
+    all_lines = []
     input_file_list = os.listdir("data/input/")
+    for filename in input_file_list:
+        file_path = os.path.join("data/input/", filename)
+        with open(file_path, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+
+    # preprocess lines
+    all_lines = [line.lower().strip() for line in all_lines]
+
+    # split in words 
+    words = []
+    for line in all_lines:
+        words.extend(word.strip(",.!?") for word in line.split())
+    
+    # count words
+    counter = {}
+    for word in words:
+        counter[word] = counter.get(word, 0) + 1
+        
+
+    write_count_words(counter)
 
     # count the frequency of the words in the files in the input directory
-    counter = {}
+    """counter = {}
     for filename in input_file_list:
         with open("data/input/" + filename) as f:
             for l in f:
                 for w in l.split():
                     w = w.lower().strip(",.!?")
-                    counter[w] = counter.get(w, 0) + 1
-
-    # create the directory output/ if it doesn't exist
-    write_count_words(counter)
-
+                    counter[w] = counter.get(w, 0) + 1"""
 
 if __name__ == "__main__":
     main()
