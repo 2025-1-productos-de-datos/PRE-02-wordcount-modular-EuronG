35f5f802f2b663ca6ddb55e8561796ed1e63577f
EuronG <juandigiraldoj@gmail.com>
2025-05-05 01:12:56 -0500

    2.2

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -11,7 +11,7 @@ def main():
 
     if len(sys.argv) != 3:
         print("need 3 args")
-        sys.exit(1)
+        sys.exit(156)
 
     input_folder = sys.argv[1]
     output_folder = sys.argv[2]
